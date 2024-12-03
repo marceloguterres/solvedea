726f1c8f396404bd9ed99a332aa5081fb7b80b35
marceloguterres <m.guterres@gmail.com>
2024-12-03 14:08:59 -0300

    Add files via upload

---
+++ streamlit_app.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="DEA Analysis",
+    layout="wide",
+    initial_sidebar_state="expanded"
+)
+
+st.title("Data Envelopment Analysis - CCR Model")
